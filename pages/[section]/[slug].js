5aa9f74d1dab96fbb854a5bfe6f08b5b66047f72
bajpai244 <bajpaiharsh244@gmail.com>
2020-10-10 02:35:59 +0530

    made some code changes

--- pages/[section]/[slug].js
+++ pages/[section]/[slug].js
@@ -1,4 +1,3 @@
-import { map } from 'lodash'
 import { Container, jsx } from 'theme-ui'
 import Error from 'next/error'
 import Header from '../../components/header/index'
@@ -7,6 +6,8 @@ import Footer from '../../components/footer/index'
 import Intro from '../../components/blogcomps/intro'
 import getAuthorImageUrl from '../../lib/data/author/index'
 import Suggestions from '../../components/blogcomps/suggestions'
+import _ from 'ramda'
+
 
 export default ({ section, slug, data, content }) => {
   if (!slug || !data) return <Error statusCode={404} />
@@ -34,17 +35,24 @@ export const getStaticPaths = () => {
 }
 
 export const getStaticProps = async ({ params }) => {
+  
   const {
     getFile,
     getBlogData,
     getBlogContent,
     getPosterUrl
   } = require('../../lib/utility/index')
+  
   const { slug, section } = params
-  const mdFile = await getFile(section, slug)
-  const data = getBlogData(mdFile)
-  data.authorimgurl = getAuthorImageUrl(data.author)
-  data.posterimgurl = getPosterUrl(section, slug, true)
-  const content = getBlogContent(mdFile)
+
+  const getData = _.pipe(await getFile, getBlogData)
+  const getContent = _.pipe(await getFile, getBlogContent)
+
+  const data = getData(section, slug)
+  const content = getContent(section, slug)
+
+  data['authorimgurl'] = getAuthorImageUrl(data.author)
+  data['posterimgurl'] = getPosterUrl(section, slug, true)
+
   return { props: { section, slug, data, content } }
 }
