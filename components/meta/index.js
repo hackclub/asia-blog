cca2a54f34d31316ff810c2048beff7b50f5c714
bajpai244 <bajpaiharsh244@gmail.com>
2020-10-27 12:58:09 +0530

    feat(add new meta): added new meta

--- components/meta/index.js
+++ components/meta/index.js
@@ -2,6 +2,26 @@ import Head from 'next/head'
 
 export default ({ title }) => (
   <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Welcome to Hack Club India's blog, it is a place to share and explore ideas and stories and know what the Hack Club community is doing!"
+    />
+
+    <meta
+      name="keywords"
+      content="hack club,hack club india,hack club india blog, blog,hack club updates, events, hack club news"
+    />
+    <meta name="author" content="Harsh Bajpai" />
+
+    <meta property="og:title" content="Hack Club India-Blog" />
+
+    <meta
+      property="og:description"
+      content="Welcome to Hack Club India's blog, it is a place to share and explore ideas and stories and know what the Hack Club community is doing!"
+    />
+
     <title>{title}</title>
+
   </Head>
 )
