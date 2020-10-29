3126a63b1ab0eb179b36f09aab263f4fcf553a7f
bajpai244 <bajpaiharsh244@gmail.com>
2020-10-29 10:54:59 +0530

    added new entry to Event Section

--- components/landingpage/verticalsection/blogpost/index.js
+++ components/landingpage/verticalsection/blogpost/index.js
@@ -14,50 +14,50 @@ export default ({
   labelStyles,
   url
 }) => (
-    <Grid
-      columns={[1, 1, 1, 2]}
-      sx={{
-        mt: index == 0 ? [4] : [4, 5],
-        px: [0, 0, 5]
-      }}
-    >
-      <Card
-        url={url}
-        posterurl={posterurl}
-        labelurl={labelurl}
-        labelStyles={labelStyles}
-      />
-      <Box sx={{ px: [0, 0, 0, 4], mt: [3, 3, 3, 0], alignSelf: 'center' }}>
-        <Box>
-          <Customh1
-            as="a"
-            href="#"
-            sx={{
-              display: 'block',
-              fontWeight: 'bold',
-              fontSize: ['2em'],
-              mb: [1, 2],
-              lineHeight: 'heading',
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              textDecoration: 'none',
-              color: 'text',
-              ':hover': { textDecorationLine: 'underline' }
-            }}
-          >
-            {title}
-          </Customh1>
-          <AuthorComp author={author} />
-          <Text sx={{ fontSize: [2, 3], mt: [4] }}>{para}</Text>
-        </Box>
-        <Box sx={{ textAlign: 'center', mt: [3] }}>
-          <Button sx={{ borderRadius: [10], px: [4], py: [2], fontSize: [2] }}>
-            READ!
-        </Button>
-        </Box>
+  <Grid
+    columns={[1, 1, 1, 2]}
+    sx={{
+      mt: index == 0 ? [4] : [4, 5],
+      px: [0, 0, 5]
+    }}
+  >
+    <Card
+      url={url}
+      posterurl={posterurl}
+      labelurl={labelurl}
+      labelStyles={labelStyles}
+    />
+    <Box sx={{ px: [0, 0, 0, 4], mt: [3, 3, 3, 0], alignSelf: 'center' }}>
+      <Box>
+        <Customh1
+          as="a"
+          href="#"
+          sx={{
+            display: 'block',
+            fontWeight: 'bold',
+            fontSize: ['2em'],
+            mb: [1, 2],
+            lineHeight: 'heading',
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            textDecoration: 'none',
+            color: 'text',
+            ':hover': { textDecorationLine: 'underline' }
+          }}
+        >
+          {title}
+        </Customh1>
+        <AuthorComp author={author} />
+        <Text sx={{ fontSize: [2, 3], mt: [4] }}>{para}</Text>
       </Box>
-    </Grid>
-  )
+      <Box sx={{ textAlign: 'center', mt: [3] }}>
+        <Button sx={{ borderRadius: [10], px: [4], py: [2], fontSize: [2] }}>
+          READ!
+        </Button>
+      </Box>
+    </Box>
+  </Grid>
+)
 
 const Customh1 = styled.h1``
